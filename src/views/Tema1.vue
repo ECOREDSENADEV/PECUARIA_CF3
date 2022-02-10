d3f6748974542533414a2977c3cef994e6239684
giorgio1966 <jhaycald@hotmail.com>
2022-02-09 23:03:38 -0500

    Segundo avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,7 +7,7 @@
     //- Una vez el componente "Muestras" no se necesite 
     //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
     //- esto evitará que se compile en la carpeta final de distribución
-    //- Muestras
+    Muestras
     .titulo-principal
       .titulo-principal__numero
         img(width="50" height="45" src='@/assets/curso/RectanguloInfo.svg')       
@@ -29,6 +29,52 @@
       .col-lg-8
         .cajon.color-acento-contenido.p-4.mb-4
           p ARTÍCULO 25. EMPRESA - CONCEPTO. Se entenderá por empresa toda actividad económica organizada para la producción, transformación, circulación, administración o custodia de bienes, o para la prestación de servicios. Dicha actividad se realizará a través de uno o más establecimientos de comercio (Código de Comercio, 1971, Artículo 25).
+    
+      h2.titulo-segundo 1.1 Clasificación
+      img(src='@/assets/curso/fondoVerduras.svg')
+
+    ImagenInfografica.color-primario.mb-5
+      template(v-slot:imagen)
+        figure
+          img(src='@/assets/template/infografiaClaseEmpresa.svg', alt='Clasificación de las empresas')
+
+      .tarjeta-avatar.color-acento-botones.p-3(x="2.8%" y="31.5%" )
+        img(src='@/assets/componentes/imagenAvatar.svg' alt='AvatarTop')
+        .tarjeta.color-secundario
+          .text-black.p-4.text-small
+            h2.text-center Según el sector económico
+            p Bajo este criterio la empresa se clasifica según el sector productivo al que pertenece su actividad principal, mediante este sistema también se suele determinar el tipo de actividad a desarrollar: de extracción, producción o comercialización.
+
+      .tarjeta-avatar.color-acento-botones.p-3(x="2.8%" y="48.5%" )
+        img(src='@/assets/componentes/imagenAvatar.svg' alt='AvatarTop')
+        .tarjeta.color-secundario
+          .text-black.p-4.text-small
+            h2.text-center Sector primario
+            p agrupa a los organismos que llevan a cabo tareas relacionadas con la explotación de recursos naturales. Este sector usualmente no involucra actividades de transformación, por lo que el cliente suele comprar los recursos tal cual fueron extraídos. Uno de los ejemplos más comunes son las empresas mineras, que extraen bienes naturales como carbón o minerales de la tierra.
+
+      .tarjeta-avatar.color-acento-botones.p-3(x="2.8%" y="63.5%" )
+        img(src='@/assets/componentes/imagenAvatar.svg' alt='AvatarTop')
+        .tarjeta.color-secundario
+          .text-black.p-4.text-small
+            h2.text-center Sector secundario
+            p incluye las empresas que desarrollan actividades manufacturares, esto significa, que adquieren materias primas de terceros para transformarlos en productos terminados con características esencialmente diferentes. Por ejemplo, una zapatería que compra insumos como madera o cuero y los transforma para producir y vender zapatos.
+
+      .tarjeta-avatar.color-acento-botones.p-3(x="2.8%" y="78.5%" )
+        img(src='@/assets/componentes/imagenAvatar.svg' alt='AvatarTop')
+        .tarjeta.color-secundario
+          .text-black.p-4.text-small
+            h2.text-center Sector terciario
+            p comprende aquellas empresas dedicadas a la intermediación de bienes sin ningún tipo de proceso o valor agregado, así como las que se dedican a la prestación de servicios. Aquí pueden incluirse empresas como las comercializadoras o supermercados que compran insumos al por mayor para luego ser vendidos por unidades. 
+
+      .tarjeta-avatar.color-acento-botones.p-3(x="23.4%" y="31.7%" )
+        img(src='@/assets/componentes/imagenAvatar.svg' alt='AvatarTop')
+        //- .tarjeta debe ir acompañado de una de una de estas clases => 
+        //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+        //- estas clases tambien tienen un modificador --borde
+        .tarjeta.color-secundario
+          .text-black.p-4.text-small
+            h2.text-center según el control de recursos
+            p Según este criterio, las empresas se clasifican según la procedencia del capital; este último entendido como la inversión efectuada por parte de sus accionistas o propietarios, para que la empresa opere. En principio existen dos fuentes principales de recursos, la proveniente del sector público, bajo la figura del Estado, y el sector privado.
 </template>
 
 <script>
